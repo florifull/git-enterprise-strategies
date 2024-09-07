9d20b499d6577e5b2c5600cb4fa234627f596475
florifull <alexskyh517@gmail.com>
2024-09-07 17:00:42 -0400

    Resolved conflict in App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import logo from './logo.svg';
+import gatorLogo from './gator.gif';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World! This is our team project.
-        </p>	
+        <img src={gatorLogo} className="App-logo" alt="logo" />
+        <p> Hello World! </p>
+        <p> My name is Alex and this is my project </p>
+        <p> This is our team project. </p>
       </header>
     </div>
   );
 }
 
 export default App;
+
